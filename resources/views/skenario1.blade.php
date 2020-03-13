30425e8f531d200c4809201bb4dbc11d3b6106a0
Rafif Shaputra <rafifshaputra25@gmail.com>
2020-03-13 16:34:03 +0700

    Add Skenario 360 Rafif

---
+++ resources/views/skenario1.blade.php
@@ -0,0 +1,407 @@
+<html>
+<head>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="js/360js/360changeImage.js"></script>
+    <script src="js/lightbox.js"></script>
+
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="/css/lightbox.css">
+    <link rel="stylesheet" href="/css/360css.css">
+
+
+</head>
+
+<body style="background-color:#F3F6F5">
+    <!--INI HALAMAN DESKTOP-->
+    <div class="desktop">
+        <!-- NAVBAR-->
+        <nav class="navbar navbar-default" style="background-color:#3A7765;">
+        <div class="container-fluid">
+            <div class="navbar-header">
+            <a class="navbar-brand" href="#" style="color:white;font-size:20px;">TRAVELRAFIF</a>
+            </div>
+
+        </div>
+        </nav>
+        <div class="contain" style="padding-top:2%;background-color:white;">
+            <!--HEADER HOTEL INFO-->
+            <div class="head" style="padding-left:3%;">
+                <div style="font-size:24px"><b> Klapa Resort</b><img style="padding-left:1%;"src="https://imgur.com/jTq0RbZ.png" width="100px;"><br></div>
+                <div style="padding-top:1%; font-size:18px;"> Jalan Uluwatu 123. Bekasi Utara. Jakarta. 17610 </div><br>
+            </div>
+
+            <!-- MAIN PICTURE-->
+            <iframe
+            width="100%"
+            allowfullscreen="allowfullscreen"
+                mozallowfullscreen="mozallowfullscreen"
+                msallowfullscreen="msallowfullscreen"
+                oallowfullscreen="oallowfullscreen"
+                webkitallowfullscreen="webkitallowfullscreen"
+            src="https://momento360.com/e/u/9303891d183c47db90e91352a9b2969d?utm_campaign=embed&utm_source=other&utm_medium=other&heading=0&pitch=0&field-of-view=75"></iframe>
+            <br>
+
+            <!--INI ROW UNTUK GAMBAR THUMBNAIL"-->
+            <div class="rows" >
+                <div class="column">
+                    <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Snow" style="width:100%;">
+                </div>
+                <div class="column">
+                    <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Forest" style="width:100%">
+                </div>
+                <div class="column">
+                    <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
+                </div>
+                <div class="column">
+                    <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
+                </div>
+            </div>
+            <br>
+            <hr>
+            <!-- ROW INFO KAMAR TERSEDIA-->
+            <div class="rows" >
+                <span>
+                    Kamar tersedia dihari yang anda pilih tersisa sebanyak <span style="color:red">3 Kamar!</span>
+                </span>
+                <span style="padding-left:30%;">
+                    <button> Pilih Kamar anda </button>
+                </span>
+                <br>
+                <br>
+                <br>
+            </div>
+        </div>
+
+        <!-- INI SECTION PILIH KAMAR-->
+        <div class="contain" style="padding-top:2%;background-color:#F3F6F5;">
+            <span style="font-size:22px;"><b>Kamar di Hotel Horison Ultima Bekasi</b> </span>
+        </div>
+        <br>
+        <div class="contain" style="background-color:white;">
+            <div class="rows" >
+                <!--KOLOM PERTAMA UNTUK FOTO KAMAR-->
+                <div class="column">
+                    <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Snow" style="width:80%;"><br>
+                    <div class="rows" style="width:80%" >
+                        <div class="column">
+                            <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Snow" style="width:100%;">
+                        </div>
+                        <div class="column">
+                            <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Forest" style="width:100%">
+                        </div>
+                        <div class="column">
+                            <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
+                        </div>
+                        <div class="column">
+                            <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
+                        </div>
+                    </div>
+                </div>
+
+                <!--KOLOM KEDUA UNTUK JENIS KAMAR DAN INFO KAMAR DAN LAYANAN-->
+                <div class="column" style="padding-left:2%;padding-top:1%">
+                    <p style="font-size:24px;"> Deluxe Twin Room </p>
+                    <br>
+
+                    <!--ROW UNTUK INFORMASI KAMAR DAN LAYANAN-->
+                    <div class="rows" style="width:100%" >
+                        <div class="column" style="font-size:18px;">
+                            <b><p style="font-size:18px">Info Kamar </b> </p>
+                            <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Max 2 Tamu <br>
+                            <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Single Bed<br>
+                            <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> 23 sqm<br>
+                        </div>
+
+                        <div class="column" style="font-size:18px;">
+                            <b><p style="font-size:18px">Layanan</b> </p>
+                            <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Max 2 Tamu <br>
+                            <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Single Bed<br>
+                            <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> 23 sqm<br>
+                        </div>
+                    </div>
+                </div>
+
+                <!--KOLOM KETIGA UNTUK KAMAR TERSISA DAN BUTTON PESAN-->
+                <div class="column" style="padding-left:10%;padding-top:10%">
+                    <span style="color:red;">Tersisa 3 kamar lagi! </span><br>
+                    <span >
+                        <button>Pesan Sekarang</button>
+                    </span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- HALAMAN UNTUK MOBILE HALAMAN AWAL -->
+    <div class="mobile" id="mobile" >
+
+        <!--navbar-->
+        <nav class="navbar navbar-default" style="background-color:#3A7765;">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                <a class="navbar-brand" href="#" style="color:white;font-size:36px;">TRAVELRAFIF</a>
+                </div>
+            </div>
+        </nav>
+
+        <!--KONTEN UTAMA-->
+        <div class="contain" style="background-color:white;">
+            <!--GAMBAR UTAMA-->
+            <iframe id="myImage" width="100%"
+            allowfullscreen="allowfullscreen"
+                mozallowfullscreen="mozallowfullscreen"
+                msallowfullscreen="msallowfullscreen"
+                oallowfullscreen="oallowfullscreen"
+                webkitallowfullscreen="webkitallowfullscreen"
+            src="https://momento360.com/e/u/9303891d183c47db90e91352a9b2969d?utm_campaign=embed&utm_source=other&utm_medium=other&heading=0&pitch=0&field-of-view=75"> </iframe>
+
+            <br>
+            <!--ROW GAMBAR THUMBNAIL-->
+            <div class="rows" >
+                <div class="column">
+                    <img id="imgcol1"onclick="imageChange()" src="https://i.ibb.co/gtKZs6z/gambar.png" alt="Snow" style="width:100%;height:180px;">
+                </div>
+                <div class="column">
+                    <img id="imgcol3" onclick="imageChange3()" src="https://i.ibb.co/0hgFCQ7/gambar3.png" alt="Mountains" style="width:100%;height:180px;">
+                </div>
+                <div class="column">
+                    <img id="imgcol4" onclick="imageChange4()"src="https://i.ibb.co/Pchrm5P/gambar4.png" alt="Mountains" style="width:100%;height:180px;">
+                </div>
+                <div class="column">
+                    <img id="imgcol2" onclick="imageChange2()"src="https://i.ibb.co/FJcb7zV/gambar2.png" alt="Forest" style="width:100%;height:180px;">
+                </div>
+            </div>
+            <br>
+
+            <!--INFORMASI HOTEL-->
+            <div class="head" style="padding-left:3%;">
+                <div style="font-size:42px"><b>Horison Ultima Bekasi</b><img style="padding-left:1%;"src="https://i.ibb.co/FWsH4nf/Group-1-1.png" width="150px;"><br></div><br>
+                <b><span style="font-size:36px"> <img src="https://imgur.com/XW23AfN.png" alt="Mountains" style="width:35px">
+                    Lokasi</b></span>
+                <div style="padding-top:1%; font-size:32px;"> Jalan Uluwatu 123. Bekasi Utara. Jakarta. 17610 </div><br>
+            </div>
+            <hr>
+            <br>
+            <!--SECTION DESKRIPSI HOTEL-->
+            <b><span style="font-size:36px">Deskripsi </span><br></b>
+            <p style="padding:15px;font-size:32px;" align="justify">  Hotel ini merupakan hotel yang terdiri dari lorem ipsum sum lorem
+                Hotel ini merupakan hotel yang terdiri dari lorem ipsum sum lorem
+                Hotel ini merupakan hotel yang terdiri dari lorem ipsum sum lorem
+                Hotel ini merupakan hotel yang terdiri dari lorem ipsum sum lorem
+            </p>
+            <hr>
+            <br>
+            <!--SECTION KEBIJAKAN HOTEL-->
+            <b><span style="font-size:36px">Kebijakan Hotel</span><br></b>
+            <ul class="a">
+                <li  style="padding:5px;font-size:32px;"> Check-in mulai dari jam 14.00</li>
+                <li  style="padding:5px;font-size:32px;"> Check-out maksimal jam 12.00</li>
+                <li  style="padding:5px;font-size:32px;"> Membawa identitas </li>
+                <li  style="padding:5px;font-size:32px;"> Deposit Rp100.000</li>
+            </ul>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <hr>
+
+            <!--STICKY BOTTOM NAVBAR-->
+            <nav class="shadow-lg navbar fixed-bottom navbar-light rounded" style="background-color:#EFECEC;padding-bottom:50px;padding-top:50px;">
+                <a class="navbar-brand" href="#" style="font-size:32px;">Hanya tersisa <span style="color:red;">3 kamar lagi! </span><span style="padding-left:15%;">
+                    <button onclick="myFunction();"> Lihat Kamar </button>
+                </span><br></a>
+            </nav>
+        </div>
+    </div>
+
+
+    <!-- HALAMAN PEMESANAN KAMAR-->
+
+    <div class="mobile2" id="mobile2" >
+        <nav class="navbar navbar-default" style="background-color:#3A7765;">
+            <div class="container-fluid">
+                <div class="navbar-header" >
+                    <div class="rows">
+                    <div class="col-sm-2" style="padding:15px;">
+                    <button onclick="myFunction()">
+                        <img src="https://imgur.com/aksZmFn.png" width="60px;">
+                    </button>
+                    </div>
+                    <div class="col-sm-10" style="padding:5px;">
+
+                            <span style="color:white;font-size:38px;"><b>Horison Ultima Bekasi</b></span><br>
+                            <span style="font-size:32px;color:white;">Bekasi, Jawa Barat</span>
+
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <br>
+
+        <!--INFORMASI SEPUTAR MENGINAP-->
+        <div class="rows" style="background-color:white;padding:15px;border-radius:10px;">
+            <div style="width:500px;height:100px;border:1px solid #4A4A4A;font-size:32px;padding:5px;" class="column3">
+            <span><b>Check-in:</b><br></span>
+            <span style="color:#3A7765;">{{  now()->toDateString('Y-m-d') }}</span>
+            </div>
+            <div style="bwidth:500px;height:100px;border:1px solid #4A4A4A;font-size:32px;padding:5px;" class="column3">
+                <span><b>Durasi:</b><br></span>
+                <span style="color:#3A7765;">2 Malam</span>
+            </div>
+            <div style="width:500px;height:100px;border:1px solid #4A4A4A;font-size:32px;padding-bottom:15px;" class="column3">
+                <span><b>Jumlah Tamu:</b><br></span>
+                <span style="color:#3A7765;">2 Tamu</span>
+            </div>
+        </div>
+        <br>
+
+
+        <div class="contain" style="padding:25px;">
+            <div style="background-color:white;border-radius:15px;">
+                <div id="demo" class="carousel slide" data-ride="carousel">
+
+                    <!-- Indicators -->
+                    <ul class="carousel-indicators">
+                      <li data-target="#demo" data-slide-to="0" class="active"></li>
+                      <li data-target="#demo" data-slide-to="1"></li>
+
+                    </ul>
+
+                    <!-- The slideshow -->
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="https://i.ibb.co/Pchrm5P/gambar4.png" onclick="openModal();currentSlide(1)" alt="Los Angeles" width="1100" height="500" class="hover-shadow cursor">>
+                      </div>
+                      <div class="carousel-item">
+                        <img src="https://i.ibb.co/CzxGHrb/20200308-130411.jpg" onclick="openModal();currentSlide(2)"alt="Chicago" width="1100" height="500" class="hover-shadow cursor">>
+                      </div>
+
+                    </div>
+
+                    <!-- Left and right controls -->
+                    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                      <span class="carousel-control-prev-icon"></span>
+                    </a>
+                    <a class="carousel-control-next" href="#demo" data-slide="next">
+                      <span class="carousel-control-next-icon"></span>
+                    </a>
+                  </div>
+            </div>
+
+            <div style="padding:10px;background-color:white;">
+                <p style="font-size:45px;padding-top:2%;padding-left:4%;"><b>Deluxe Room</b><br>
+                <hr size="100">
+                </p>
+                <br>
+            </div>
+            <div style="padding:2px;background-color:white;">
+                <div class="rows" style="width:100%;padding-left:4%;"  >
+                    <div class="column4" style="font-size:34px;">
+                        <b><p style="font-size:36px">Info Kamar </b> </p>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Max 2 Tamu <br>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Single Bed<br>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> 23 sqm<br>
+                    </div>
+                    <div class="column4" style="font-size:34px;">
+                        <b><p style="font-size:36px">Layanan</b> </p>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Without Breakfast <br>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Non Refundable<br>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Free Wifi<br>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <span class="navbar-brand" href="#" style="font-size:32px;padding-left:4%;">Hanya tersisa <span style="color:red;">3 kamar lagi! </span></span>
+                <br><br><br>
+                <div class="rows" >
+                    <span style="font-size:36px;padding-left:4%;padding-top:2%">
+                        LIHAT DETAIL</span>
+                    </span>
+                    <span style="margin-left:35%">
+                        <button class="but"> Pesan Sekarang </button>
+                    </span>
+                </div>
+                <br><br>
+            </div>
+            <br>
+            <br>
+        </div>
+    </div>
+<br>
+<br>
+<!-- The Modal/Lightbox -->
+<div id="myModal" class="modal">
+    <span class="close cursor" onclick="closeModal()">&times;</span>
+    <div class="modal-content">
+
+      <div class="mySlides">
+        <div class="numbertext">1 / 2</div>
+        <iframe
+            width="100%" style="height:500px;"
+            allowfullscreen="allowfullscreen"
+                mozallowfullscreen="mozallowfullscreen"
+                msallowfullscreen="msallowfullscreen"
+                oallowfullscreen="oallowfullscreen"
+                webkitallowfullscreen="webkitallowfullscreen"
+            src="https://momento360.com/e/u/55df697591334df5a53db4b3fa6ea9cd?utm_campaign=embed&utm_source=other&utm_medium=other&heading=0&pitch=0&field-of-view=75"></iframe>
+      </div>
+
+      <div class="mySlides">
+        <div class="numbertext">2 / 2</div>
+        <iframe
+            width="100%" style="height:500px;"
+            allowfullscreen="allowfullscreen"
+                mozallowfullscreen="mozallowfullscreen"
+                msallowfullscreen="msallowfullscreen"
+                oallowfullscreen="oallowfullscreen"
+                webkitallowfullscreen="webkitallowfullscreen"
+            src="https://momento360.com/e/u/3c26621f3fc74286a6c32dc0769e3ee7?utm_campaign=embed&utm_source=other&utm_medium=other&heading=0&pitch=0&field-of-view=75"></iframe>
+      </div>
+
+
+
+      <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+      <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+      <div class="caption-container">
+        <p id="caption"></p>
+      </div>
+      <div class="rows">
+      <div class="column">
+        <img class="demo cursor" src="https://i.ibb.co/Pchrm5P/gambar4.png" style="width:100%;height:200px;"  onclick="currentSlide(1)" alt="Kamar Tidur">
+      </div>
+      <div class="column" >
+        <img class="demo cursor" src="https://i.ibb.co/CzxGHrb/20200308-130411.jpg" style="width:100%;height:200px;" onclick="currentSlide(2)" alt="Kamar Mandi">
+      </div>
+    </div>
+
+
+    </div>
+  </div>
+<script>
+function myFunction() {
+  var x = document.getElementById("mobile");
+  var y = document.getElementById("mobile2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none"
+  } else {
+    y.style.display = "block";
+    x.style.display = "none";
+
+  }
+}
+</script>
+</body>
+<br><br><br><br><br>
+</html>
