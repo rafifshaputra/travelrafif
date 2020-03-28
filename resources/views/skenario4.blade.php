cb28a0de44371c5edfdbd1db1507a6076618a5ca
Rafif Shaputra <rafifshaputra25@gmail.com>
2020-03-29 01:09:31 +0700

    change gif to link

--- resources/views/skenario4.blade.php
+++ resources/views/skenario4.blade.php
@@ -154,7 +154,7 @@
     </nav>
     <div class="contain" style="background-color:white;">
 
-        <img id="myImage" width="100%" height="700px"src="hrrestaurantgif.gif">
+        <img id="myImage" width="100%" height="700px"src="https://s5.gifyu.com/images/hrrestaurantgif.gif">
         <br>
         <br>
         <!--ROW GAMBAR THUMBNAIL-->
