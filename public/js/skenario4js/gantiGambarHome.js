3719ba1434ba12c4968529e450700dc1d2030f57
Rafif Shaputra <rafifshaputra25@gmail.com>
2020-03-29 03:17:18 +0700

    add loading screen transitions

--- public/js/skenario4js/gantiGambarHome.js
+++ public/js/skenario4js/gantiGambarHome.js
@@ -1,15 +1,15 @@
 function gambarGanti1(){
-    y="hrlobi1.gif";
+    y="https://s5.gifyu.com/images/hrlobi1.gif";
+    document.getElementById('myImage').src="";
     document.getElementById('myImage').src=y;
     document.getElementById('imgcol1').style.opacity = "1";
     document.getElementById('imgcol2').style.opacity = "0.6";
     document.getElementById('imgcol3').style.opacity = "0.6";
     document.getElementById('imgcol4').style.opacity = "0.6";
 }
-
-
 function gambarGanti2(){
-    y="hrkolamrenang1.gif";
+    y="https://s5.gifyu.com/images/hrkolamrenang1.gif";
+    document.getElementById('myImage').src="";
     document.getElementById('myImage').src=y;
     document.getElementById('imgcol1').style.opacity = "0.6";
     document.getElementById('imgcol2').style.opacity = "1";
